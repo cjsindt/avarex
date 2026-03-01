ef064217693c5c048e6a812e891982a4da91fd3c
Connor Sindt <connor.sindt@gmail.com>
2026-03-01 12:03:46 -0600

    else instead of break

--- lib/chart/download_screen.dart
+++ lib/chart/download_screen.dart
@@ -297,24 +297,25 @@ class DownloadScreenState extends State<DownloadScreen> {
         if (chart.progress.value > 0 && chart.progress.value < 100) {
           selectedCharts++;
           inProgress = inProgress || true;
-          break;
         }
-        switch (chart.state) {
-          case _stateExpiredNone:
-            expired = expired || true;
-            break;
-          case _stateCurrentNone:
-            current = current || true;
-            break;
-          case _stateAbsentDownload:
-          case _stateExpiredDownload:
-          case _stateExpiredDelete:
-          case _stateCurrentDownload:
-          case _stateCurrentDelete:
-            selectedCharts++;
-            inProgress = inProgress || true;
-          default:
-            break;
+        else {
+          switch (chart.state) {
+            case _stateExpiredNone:
+              expired = expired || true;
+              break;
+            case _stateCurrentNone:
+              current = current || true;
+              break;
+            case _stateAbsentDownload:
+            case _stateExpiredDownload:
+            case _stateExpiredDelete:
+            case _stateCurrentDownload:
+            case _stateCurrentDelete:
+              selectedCharts++;
+              inProgress = inProgress || true;
+            default:
+              break;
+          }
         }
       }
 
